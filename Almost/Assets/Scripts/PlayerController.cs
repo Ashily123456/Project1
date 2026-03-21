0486073d1c480ebe0099652bb48ad76bfed67a8a
Ashily123456 <smzysrtwyzw2351310620@gmail.com>
2026-03-21 13:54:18 +0800

    Adding prototype

---
+++ Almost/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float speed = 0.1f;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector3(0, 0, 1 * speed);
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            rb.velocity = Vector3.zero;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = new Vector3(0, 0, -1 * speed);
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            rb.velocity = Vector3.zero; 
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector3(-1, 0, 0 * speed);
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            rb.velocity = Vector3.zero;
+        }
+        
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector3(1, 0, 0 * speed);
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
+
+
